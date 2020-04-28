24b8c8a45c2a10e666be4f0e6ef094193b96e797
wallzack <vulcheek@gmail.com>
2020-04-28 14:27:56 +0200

    25.4 moved passport conf to seperate file

--- server.js
+++ server.js
@@ -3,32 +3,14 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportConfig = require('./config/passport');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.clientID,
-  clientSecret: process.env.clientSecret,
-  callbackURL: process.env.callbackURL,
-}, (accessToken, refreshToken, profile, done) => {
-done(null, profile);
-}));
-
-app.use(session({ secret: 'anything' }));
+app.use(session({ secret: 'authbase project session string' }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-  serialize(null, user);
-});
-
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-  deserialize(null, obj);
-});
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -42,6 +24,15 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
+
 app.get('/user/logged', (req, res) => {
   res.render('logged');
 });
@@ -49,16 +40,6 @@ app.get('/user/logged', (req, res) => {
 app.get('/user/no-permission', (req, res) => {
   res.render('noPermission');
 });
-
-app.get('/auth/google', 
-  passport.authenticate('google', { scope: ['email', 'profile'] 
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
-);
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
